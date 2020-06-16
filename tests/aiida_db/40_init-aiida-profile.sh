0cbc2f3a6ce2b7fc7c3131e390075c294c3faa11
Chris Sewell <chrisj_sewell@hotmail.com>
2020-06-16 11:24:38 +0100

    update aiida-core setup

--- tests/aiida_db/40_init-aiida-profile.sh
+++ tests/aiida_db/40_init-aiida-profile.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
-tar -xjf /etc/my_init.d/aiida_archive.tar.bz2 -C /home/aiida/.aiida
+# only unzip the first time, if we restart the container
+if [[ ! -f /opt/UNZIP_COMPLETED ]] ; then
+    tar -xjf /etc/my_init.d/aiida_archive.tar.bz2 -C /home/aiida/.aiida
+    touch /opt/UNZIP_COMPLETED
+fi
